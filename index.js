69b6f1b6a674a3ed276ba79f3caab1ee7e39bc6c
nurulhodanahid11 <61221153+nurulhodanahid11@users.noreply.github.com>
2021-08-22 19:14:40 +0600

    mongo client complete

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Get
 
 app.get('/', (req, res) => {
-    res.send("Thank you for calling me")
+    const fruit = {
+        product: "ada",
+        price: 220
+    }
+    res.send(fruit)
   })
+
+app.get("/fruit/banana", (req, res) => {
+    res.send({name:"banana", quantity:1000, price:10000})
+})
+
+const users = ["Steven", "Smith", "Aston", "David", "Shakib"];
+
+app.get("/user/:id", (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id , name});
+    // console.log(req.query.sort)
+})
+
+// Post
+
+app.post("/addUser", (req, res) => {
+    //save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user)
+})
   
 app.listen(3000, () => console.log("Listening to port 3000"))
